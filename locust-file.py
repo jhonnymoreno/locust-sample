390302230150a1c55f453b44321d0a5982a5aa84
jmoreno <jmoreno@ciandt.com>
2021-08-02 22:44:04 -0300

    moving on_start fn for better understanding

--- locust-file.py
+++ locust-file.py
@@ -6,6 +6,9 @@ from locust import HttpUser, task, between
 class DemoUser(HttpUser):
     wait_time = between(1,3)
 
+    def on_start(self):
+        ...
+        
     @task
     def city_info(self):
         self.client.get("/ma/belmont", name="/city-info")
@@ -15,6 +18,3 @@ class DemoUser(HttpUser):
         zip_code = random.randrange(23401,23800)
         self.client.get(f"/{zip_code}", name="/zip-info")
         time.sleep(5)
-    
-    def on_start(self):
-        ...
